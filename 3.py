e62cc2f691e8ae9adb0a3cb8d04b6460382a8f05
OlzhasDuisenbay <o_duisenbay@kbtu.kz>
2025-04-04 18:31:54 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,36 +1,35 @@
-"""Draw circle - a red ball of size 50 x 50 (radius = 25) on white background.
-When user presses Up, Down, Left, Right arrow keys on keyboard, the ball should move by 20 pixels in the direction of pressed key.
-The ball should not leave the screen, i.e. user input that leads the ball to leave of the screen should be ignored"""
 import pygame
-
 pygame.init()
 
-WIDTH, HEIGHT = 800, 400
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+monitor = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("PP2 Pygame")
 
-x, y = WIDTH // 2, HEIGHT // 2
-radius = 25
+x = 400
+y = 200
 speed = 20
 
-running = True
-while running:
-    screen.fill("White")
-    pygame.draw.circle(screen, "Red", (x, y), radius)
+check = True
+
+while check:
+    monitor.fill("White")
+    pygame.draw.circle(monitor, 'Red', (x, y), 25)
     pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    for action in pygame.event.get():
+        if action.type == pygame.QUIT:
+            check = False
+            pygame.quit()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x - speed >= radius:
-        x -= speed
-    if keys[pygame.K_RIGHT] and x + speed <= WIDTH - radius:
-        x += speed
-    if keys[pygame.K_UP] and y - speed >= radius:
-        y -= speed
-    if keys[pygame.K_DOWN] and y + speed <= HEIGHT - radius:
-        y += speed
+        keys = pygame.key.get_pressed()
 
-pygame.quit()
+        if keys[pygame.K_LEFT] and x - speed > 0 + 20:
+            x -= speed
+        if keys[pygame.K_RIGHT] and x + speed < 800 - 20:
+            x += speed
+        if keys[pygame.K_UP] and y - speed > 0 + 20:
+            y -= speed
+        if keys[pygame.K_DOWN] and y + speed < 400 - 20:
+            y += speed
+
+        pygame.draw.circle(monitor, 'Red', (x, y), 25)
+        pygame.display.update()
